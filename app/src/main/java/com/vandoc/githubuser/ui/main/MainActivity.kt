35e20568c18da2ffab5594c0d7105aa01a0ddfe1
vandoc <ichvandiocta@gmail.com>
2021-09-02 11:19:19 +0700

    Subscribe users data on viewmodel

--- app/src/main/java/com/vandoc/githubuser/ui/main/MainActivity.kt
+++ app/src/main/java/com/vandoc/githubuser/ui/main/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.vandoc.githubuser.ui.main
 
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.vandoc.githubuser.R
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    mainViewModel.users.collect {
+                        Log.e("Main", it.toString())
+                    }
+                }
+
+                launch {
+                    mainViewModel.uiState.collect {
+                        Log.e("MainState", it.toString())
+                    }
+                }
+            }
+        }
     }
 }
