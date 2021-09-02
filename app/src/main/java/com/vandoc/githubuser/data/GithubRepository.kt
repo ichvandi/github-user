cf3183fbbb75352ad8bd78593a768dc2cb1c23a6
vandoc <ichvandiocta@gmail.com>
2021-09-02 11:17:25 +0700

    Refactor repository

--- app/src/main/java/com/vandoc/githubuser/data/GithubRepository.kt
+++ app/src/main/java/com/vandoc/githubuser/data/GithubRepository.kt
@@ -3,12 +3,10 @@ package com.vandoc.githubuser.data
 import com.vandoc.githubuser.data.remote.RemoteRepository
 import com.vandoc.githubuser.data.util.Resource
 import com.vandoc.githubuser.model.User
-import com.vandoc.githubuser.util.toMap
+import com.vandoc.githubuser.util.mapResource
 import com.vandoc.githubuser.util.toModel
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -21,23 +19,8 @@ class GithubRepository @Inject constructor(private val remoteRepository: RemoteR
     override suspend fun getUsers(page: Int, perPage: Int): Flow<Resource<List<User>>> {
         return flow {
             emit(Resource.Loading)
-
-            when (val response = remoteRepository.getUsers(page, perPage)) {
-                is Resource.Success -> {
-                    val responseDetail = response.data.asFlow().flatMapMerge {
-                        flow {
-                            emit(it.username to remoteRepository.getUserDetail(it.username))
-                        }
-                    }.toMap()
-
-                    val usernames = response.data.map { it.username }
-                    val users = usernames.map { (responseDetail[it]!! as Resource.Success).data }
-
-                    emit(Resource.Success(users.map { it.toModel() }))
-                }
-                is Resource.Error -> emit(response)
-                Resource.Loading -> emit(Resource.Loading)
-            }
-        }
+            val response = remoteRepository.getUsers(page, perPage)
+            emit(response)
+        }.mapResource { response -> response.map { it.toModel() } }
     }
 }
--- app/src/main/java/com/vandoc/githubuser/data/remote/RemoteRepository.kt
+++ app/src/main/java/com/vandoc/githubuser/data/remote/RemoteRepository.kt
@@ -1,35 +1,42 @@
 package com.vandoc.githubuser.data.remote
 
-import com.vandoc.githubuser.data.remote.response.UserMinimalResponse
 import com.vandoc.githubuser.data.remote.response.UserResponse
 import com.vandoc.githubuser.data.util.Resource
 import com.vandoc.githubuser.util.Helper
 import com.vandoc.githubuser.util.getError
+import com.vandoc.githubuser.util.toMap
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class RemoteRepository @Inject constructor(private val service: GithubService) {
 
-    suspend fun getUsers(page: Int, perPage: Int): Resource<List<UserMinimalResponse>> {
+    @OptIn(FlowPreview::class)
+    suspend fun getUsers(page: Int, perPage: Int): Resource<List<UserResponse>> {
         return Helper.runCatching {
             val response = service.getUsers(page, perPage)
             if (response.isSuccessful) {
-                response.body() ?: Error("An error occurred!")
+                val usersMin = response.body() ?: return@runCatching Error("An error occurred!")
+                val responseDetail = usersMin.asFlow().flatMapMerge {
+                    flow {
+                        emit(it.username to service.getUserDetail(it.username))
+                    }
+                }.toMap()
+
+                val usernames = usersMin.map { it.username }
+                val data = usernames.map { responseDetail[it]!! }
+                val users = data.filter { it.isSuccessful }.map { it.body()!! }
+                val errors = data.filter { !it.isSuccessful }.map { it.getError() }
+
+                if (errors.isNotEmpty()) errors else users
             } else {
                 response.getError()
             }
         }
     }
 
-    suspend fun getUserDetail(username: String): Resource<UserResponse> {
-        return Helper.runCatching {
-            val response = service.getUserDetail(username)
-            if (response.isSuccessful) {
-                response.body() ?: Error("An error occurred!")
-            } else {
-                response.getError()
-            }
-        }
-    }
 }
