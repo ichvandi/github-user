b33e50fd2b505dfac152fea616767c314f82804c
vandoc <ichvandiocta@gmail.com>
2021-09-02 07:55:09 +0700

    Remove unused extensions

--- app/src/main/java/com/vandoc/githubuser/util/Mapper.kt
+++ app/src/main/java/com/vandoc/githubuser/util/Mapper.kt
@@ -1,15 +1,7 @@
 package com.vandoc.githubuser.util
 
-import com.vandoc.githubuser.data.remote.response.UserMinimalResponse
 import com.vandoc.githubuser.data.remote.response.UserResponse
 import com.vandoc.githubuser.model.User
-import com.vandoc.githubuser.model.UserMinimal
-
-fun UserMinimalResponse.toModel() = UserMinimal(
-    id = id,
-    username = username,
-    avatar = avatar
-)
 
 fun UserResponse.toModel() = User(
     id = id,
