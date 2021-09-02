c6f5be2c0a2e960e42c11667f8d0b097d1cc3bd8
vandoc <ichvandiocta@gmail.com>
2021-09-02 11:17:04 +0700

    Location response can be null

--- app/src/main/java/com/vandoc/githubuser/data/remote/response/UserResponse.kt
+++ app/src/main/java/com/vandoc/githubuser/data/remote/response/UserResponse.kt
@@ -15,7 +15,7 @@ data class UserResponse(
     val id: Int,
     @Json(name = "login") val username: String,
     @Json(name = "avatar_url") val avatar: String,
-    val location: String,
+    val location: String?,
     val email: String?,
     @Json(name = "created_at") val createdAt: String
 )
