66d96ebdb142fbd71ca6561941bedf1401d72cc3
vandoc <ichvandiocta@gmail.com>
2021-09-02 07:53:25 +0700

    Refactor repository

--- app/src/main/java/com/vandoc/githubuser/data/GithubDataSource.kt
+++ app/src/main/java/com/vandoc/githubuser/data/GithubDataSource.kt
@@ -2,12 +2,10 @@ package com.vandoc.githubuser.data
 
 import com.vandoc.githubuser.data.util.Resource
 import com.vandoc.githubuser.model.User
-import com.vandoc.githubuser.model.UserMinimal
 import kotlinx.coroutines.flow.Flow
 
 interface GithubDataSource {
 
-    suspend fun getUsers(page: Int, perPage: Int): Flow<Resource<List<UserMinimal>>>
+    suspend fun getUsers(page: Int, perPage: Int): Flow<Resource<List<User>>>
 
-    suspend fun getUserDetail(username: String): Flow<Resource<User>>
 }
--- app/src/main/java/com/vandoc/githubuser/data/GithubRepository.kt
+++ app/src/main/java/com/vandoc/githubuser/data/GithubRepository.kt
@@ -3,10 +3,12 @@ package com.vandoc.githubuser.data
 import com.vandoc.githubuser.data.remote.RemoteRepository
 import com.vandoc.githubuser.data.util.Resource
 import com.vandoc.githubuser.model.User
-import com.vandoc.githubuser.model.UserMinimal
-import com.vandoc.githubuser.util.mapResource
+import com.vandoc.githubuser.util.toMap
 import com.vandoc.githubuser.util.toModel
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -15,19 +17,27 @@ import javax.inject.Singleton
 class GithubRepository @Inject constructor(private val remoteRepository: RemoteRepository) :
     GithubDataSource {
 
-    override suspend fun getUsers(page: Int, perPage: Int): Flow<Resource<List<UserMinimal>>> {
+    @OptIn(FlowPreview::class)
+    override suspend fun getUsers(page: Int, perPage: Int): Flow<Resource<List<User>>> {
         return flow {
             emit(Resource.Loading)
-            val response = remoteRepository.getUsers(page, perPage)
-            emit(response)
-        }.mapResource { response -> response.map { it.toModel() } }
-    }
 
-    override suspend fun getUserDetail(username: String): Flow<Resource<User>> {
-        return flow {
-            emit(Resource.Loading)
-            val response = remoteRepository.getUserDetail(username)
-            emit(response)
-        }.mapResource { it.toModel() }
+            when (val response = remoteRepository.getUsers(page, perPage)) {
+                is Resource.Success -> {
+                    val responseDetail = response.data.asFlow().flatMapMerge {
+                        flow {
+                            emit(it.username to remoteRepository.getUserDetail(it.username))
+                        }
+                    }.toMap()
+
+                    val usernames = response.data.map { it.username }
+                    val users = usernames.map { (responseDetail[it]!! as Resource.Success).data }
+
+                    emit(Resource.Success(users.map { it.toModel() }))
+                }
+                is Resource.Error -> emit(response)
+                Resource.Loading -> emit(Resource.Loading)
+            }
+        }
     }
 }
