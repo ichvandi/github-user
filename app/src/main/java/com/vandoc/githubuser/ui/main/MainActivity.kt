0e00a58861c420338658911a93fccc1801a60c1a
vandoc <ichvandiocta@gmail.com>
2021-09-02 07:59:26 +0700

    Move MainActivity.kt to ui/main package

--- app/src/main/java/com/vandoc/githubuser/ui/main/MainActivity.kt
+++ app/src/main/java/com/vandoc/githubuser/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.vandoc.githubuser
+package com.vandoc.githubuser.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.vandoc.githubuser.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
