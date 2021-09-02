fcc76bcb91c0fd8d36c762b69f69685fca0a8524
vandoc <ichvandiocta@gmail.com>
2021-09-02 07:06:41 +0700

    Add domain model

---
+++ app/src/main/java/com/vandoc/githubuser/model/User.kt
@@ -0,0 +1,20 @@
+package com.vandoc.githubuser.model
+
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class UserMinimal(
+    val id: Int,
+    val username: String,
+    val avatar: String
+)
+
+@JsonClass(generateAdapter = true)
+data class User(
+    val id: Int,
+    val username: String,
+    val avatar: String,
+    val location: String,
+    val email: String?,
+    val createdAt: String
+)
