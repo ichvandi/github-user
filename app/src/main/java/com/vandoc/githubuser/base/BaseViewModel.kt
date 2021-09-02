c17169f05c5663cdbc621d6bc65708cb2392743e
vandoc <ichvandiocta@gmail.com>
2021-09-02 07:54:00 +0700

    Add BaseViewModel.kt

---
+++ app/src/main/java/com/vandoc/githubuser/base/BaseViewModel.kt
@@ -0,0 +1,17 @@
+package com.vandoc.githubuser.base
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+
+abstract class BaseViewModel : ViewModel() {
+    protected val _uiState = Channel<UiState>()
+    val uiState = _uiState.receiveAsFlow()
+
+    sealed class UiState {
+        data class Success(val message: String?) : UiState()
+        data class Error(val message: String) : UiState()
+        object Loading : UiState()
+    }
+
+}
